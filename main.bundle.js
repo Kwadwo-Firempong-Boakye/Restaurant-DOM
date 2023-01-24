70d5348b4ba5ebf008c6816080065c5310d15e73
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-01-24 19:40:48 +0000

    Create module bundle via webpack

---
+++ main.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Everything is working");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxxQyIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtZG9tLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiRXZlcnl0aGluZyBpcyB3b3JraW5nXCIpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
