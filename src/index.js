baefda940a7068d91df2c49272bdb8cd19ab9cff
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-01-25 14:48:32 +0000

    Comment out image imports for webpack bundling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import './index.css';
+// import hamburger from './ahamburger.jpg';
+// import burger from './hero-burger.jpeg';
+// import cheese from './cheese-burger.png';
+// import cheeseo from './cheese-burger-o.png';
 console.log("Everything is working");
