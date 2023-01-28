4f3832c802dab39c632c4bdbd5174a5601aba8db
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-01-28 16:22:17 +0000

    Bundle CSS &Add hamburger menu logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import './index.css';
-// import hamburger from './ahamburger.jpg';
-// import burger from './hero-burger.jpeg';
-// import cheese from './cheese-burger.png';
-// import cheeseo from './cheese-burger-o.png';
-console.log("Everything is working");
+import "./index.css";
+
+const homePageDOM = (() => {
+	const hamburger = document.querySelector(".hamburger");
+	const mobileMenu = document.querySelector(".new-menu");
+	return { hamburger, mobileMenu };
+})();
+
+const homePageEvents = (() => {
+	homePageDOM.hamburger.addEventListener("click", (e) => {
+		showMobileMenu(e);
+	});
+	window.addEventListener("click", hideMobileMenu);
+})();
+
+function showMobileMenu(e) {
+	e.stopPropagation();
+	if (homePageDOM.mobileMenu.style.top == "-500px") {
+		homePageDOM.mobileMenu.style.top = "96px";
+	} else {
+		homePageDOM.mobileMenu.style.top = "-500px";
+	}
+}
+
+function hideMobileMenu() {
+	if (homePageDOM.mobileMenu.style.top != "-500px") {
+		homePageDOM.mobileMenu.style.top = "-500px";
+	}
+}
