0b1639210c7888b74a6cf0b115b5de3426f56f34
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-01-28 16:20:35 +0000

    Reconfigure webpack to not use html plugin & clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	mode: "development",
 	entry: "./src/index.js",
 	devtool: "inline-source-map",
 	output: {
-		filename: "[name].bundle.js",
+		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
-		// clean: true,
 	},
 	module: {
 		rules: [
