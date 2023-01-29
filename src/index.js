2e96d2e504b7cd11c017986535da570adddec25c
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-01-29 01:29:30 +0000

    Create HomePage generation function

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import "./index.css";
+import createNav from "./create-nav";
+import createHero from "./create-hero";
 
-const homePageDOM = (() => {
-	const hamburger = document.querySelector(".hamburger");
-	const mobileMenu = document.querySelector(".new-menu");
-	return { hamburger, mobileMenu };
-})();
+const createHomePage = () => {
+	createNav();
+	createHero();
+};
 
-const homePageEvents = (() => {
-	homePageDOM.hamburger.addEventListener("click", (e) => {
-		showMobileMenu(e);
-	});
-	window.addEventListener("click", hideMobileMenu);
-})();
-
-function showMobileMenu(e) {
-	e.stopPropagation();
-	if (homePageDOM.mobileMenu.style.top == "-500px") {
-		homePageDOM.mobileMenu.style.top = "96px";
-	} else {
-		homePageDOM.mobileMenu.style.top = "-500px";
-	}
-}
-
-function hideMobileMenu() {
-	if (homePageDOM.mobileMenu.style.top != "-500px") {
-		homePageDOM.mobileMenu.style.top = "-500px";
-	}
-}
+window.addEventListener("load", createHomePage);
